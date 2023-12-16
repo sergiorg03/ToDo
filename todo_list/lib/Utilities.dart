87f59cbb6fc3a7435493836bcf852a403b33c3f5
Sergio Rodriguez <srodgen0910@gmail.com>
2023-12-16 23:10:23 +0100

    corrected some irrelevant check

--- todo_list/lib/Utilities.dart
+++ todo_list/lib/Utilities.dart
@@ -1,26 +1,24 @@
 class Utilities {
+  
   static const IP = "";
   static const URL = "";
+
    List<String> LETRAS_DNI = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"];
 
-   /**
-    * Función que comprueba si una cadena contiene texto o no.
-    * @return {bool} -- Devuelve True si la cadena pasada por parametro contiene texto. False si no.
-    */
+   /// Función que comprueba si una cadena contiene texto o no.
+   /// @return {bool} -- Devuelve True si la cadena pasada por parametro contiene texto. False si no.
    bool contieneTexto(String cadena){
      bool contiene = false;
 
-     if(cadena.isNotEmpty && cadena != null && cadena != ''){
+     if(cadena.isNotEmpty && cadena != ''){
        contiene = true;
      }
 
      return contiene;
    }
 
-   /**
-    * Función que comprueba si el DNI introducido tiene el formato correcto.
-    * @return {bool} -- Devuelve True si el dni es correcto, False si no.
-    */
+   /// Función que comprueba si el DNI introducido tiene el formato correcto.
+   /// @return {bool} -- Devuelve True si el dni es correcto, False si no.
    bool comprobarDni (String dni){
      bool valido = false;
 
@@ -31,10 +29,8 @@ class Utilities {
      return valido;
    }
 
-   /**
-    * Funcion que comprueba si una cadena es numerica o no
-    * @return {bool} -- Devuelve true si una cadena introducida es númerica. True si lo es, False si no.
-    */
+   /// Funcion que comprueba si una cadena es numerica o no
+   /// @return {bool} -- Devuelve true si una cadena introducida es númerica. True si lo es, False si no.
    bool esNumerico (String cadena){
      bool valido = false;
      
